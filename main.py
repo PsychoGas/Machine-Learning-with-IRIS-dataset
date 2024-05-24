5a41d971c9e20105abd1afa4c78bb67f2b8d5c8b
Psycho <psychogas69@gmail.com>
2024-05-24 12:09:04 +0530

    Implement 6 different models and test which one has highest accuracy, with the help of a box plot

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+
+# Importing the models 
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -22,14 +24,60 @@ url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
+# Split-out validation dataset -> load the dataset into a numpy array
+array = dataset.values
 
-# Print number of rows and columns in datatset - shape
-print(dataset.shape)
+# sepal length, sepal width, petal length, petal width will be one array X
+X = array[:,0:4]
+
+# y will have all the fifth column of all rows -> flower type -> result
+y = array[:,4]
 
 
-# Peek at the data - head
-print(dataset.head(20))
+# We will use 80% of data to train and 20% to validate the model
+"""
+X_train: 80% of the feature data used for training the model.
+X_validation: 20% of the feature data used for validating/testing the model.
+Y_train: 80% of the target data used for training the model.
+Y_validation: 20% of the target data used for validating/testing the model.
+"""
+
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
 
 
-# Checking number of rows related to a specific class - groupby
-print(dataset.groupby('class').size())
+# We will use stratified 10 fold cross validation to train the model and test it
+# Note that when we split the data, we used a random_state = 1 to make the split reproducible i.e the same split will be obtained if we run the code again hence the cross validation will be the same
+
+# Accuracy -> ratio of the number of correctly predicted instances divided by the total number of instances in the dataset multiplied by 100
+
+"""
+Algorithms:
+Logistic Regression (LR): Finds a rule (line or curve) to separate two groups.
+Linear Discriminant Analysis (LDA): Finds directions to separate multiple groups.
+K-Nearest Neighbors (KNN): Looks at the nearest balls and decides based on their majority color.
+Classification and Regression Trees (CART): Asks a series of questions to split the data into groups.
+Gaussian Naive Bayes (NB): Uses probabilities to guess the color.
+Support Vector Machines (SVM): Finds the best line or curve that separates the groups with the widest gap.
+"""
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+ kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+ cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+ results.append(cv_results)
+ names.append(name)
+ print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+# Compare Algorithms
+plt.boxplot(results, labels=names)
+plt.title('Algorithm Comparison')
+plt.show()
