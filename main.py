2e3401383802738a88f334db8f72d917be5bc7f3
Psycho <psychogas69@gmail.com>
2024-05-24 10:47:53 +0530

    Load data set and review data

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+
+...
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+
+# Print number of rows and columns in datatset - shape
+print(dataset.shape)
+
+
+# Peek at the data - head
+print(dataset.head(20))
+
+
+# Checking number of rows related to a specific class - groupby
+print(dataset.groupby('class').size())
