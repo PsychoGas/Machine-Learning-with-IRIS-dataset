36a2f08bbb26cbcf948be372f0c26ad760e61704
Psycho <psychogas69@gmail.com>
2024-05-24 14:17:57 +0530

    final changes

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn.svm import SVC
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
+
 # Split-out validation dataset
 array = dataset.values
 X = array[:,0:4]
@@ -20,6 +21,10 @@ X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=
 model = SVC(gamma='auto')
 model.fit(X_train, Y_train)  # Training the model with input n output
 predictions = model.predict(X_validation)  # Making the model predict output using our tetsing data (20% of the dataset) and store the results in predictions
+
+for i, j in (zip(predictions, Y_validation)):
+    if (i != j):
+        print("Predicted: ", i, "     -       Actual: ", j)
 
 # Evaluate predictions score
 print(accuracy_score(Y_validation, predictions) * 100, "%")
